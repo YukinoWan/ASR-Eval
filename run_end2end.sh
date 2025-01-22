23125f9a4653118bf9b248b945ac47ad69ca83cd
zhenwan.nlp <zhenwan.nlp@cnode7-014.cm.cluster>
2025-01-22 22:50:44 +0800

    run script

--- run_end2end.sh
+++ run_end2end.sh
@@ -1,1 +1,4 @@
-python end2end_asr.py "medasr" "desta2"
+python end2end_asr.py "medasr" "gemini-1.5-flash"
+python end2end_asr.py "earning22" "gemini-1.5-flash"
+python end2end_asr.py "tedlium" "gemini-1.5-flash"
+python end2end_asr.py "voxpopuli" "gemini-1.5-flash"
