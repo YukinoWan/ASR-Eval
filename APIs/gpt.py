67310ca848a38fa1a107fd1d3b750d3552cd60b5
zhenwan.nlp <zhenwan.nlp@154-T2-P1-NVR.cm.cluster>
2025-02-05 01:26:49 +0800

    vicuna

--- APIs/gpt.py
+++ APIs/gpt.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 def generate_gpt_response(model, input_text):
-    client = OpenAI(api_key="")
+    client = OpenAI(api_key="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")
     completion = client.chat.completions.create(
         model=model,
         messages=[
@@ -14,4 +14,3 @@ def generate_gpt_response(model, input_text):
 
 
 
-# print(generate_gpt_response("gpt-4o", "请给我讲3个笑话"))
--- run_answer.sh
+++ run_answer.sh
@@ -17,7 +17,7 @@
 # python answer_qa.py "tedlium" "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "gpt" "gpt-4o"
 # python answer_qa.py "voxpopuli" "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "gpt" "gpt-4o"
 
-python answer_qa.py "voxpopuli" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
-python answer_qa.py "earning22" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
-python answer_qa.py "medasr" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
-python answer_qa.py "tedlium" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
+python answer_qa.py "voxpopuli" "whisper-large-v2" "whisper_v2_1best" "vicuna" "lmsys/vicuna-13b-v1.1"
+#python answer_qa.py "earning22" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
+#python answer_qa.py "medasr" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
+#python answer_qa.py "tedlium" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
