9921c7662160c756c5f8300f1a8d1d1a4b42964c
zhenwan.nlp <zhenwan.nlp@cnode7-013.cm.cluster>
2025-02-06 20:35:33 +0800

    whisperv3-qwen2

--- run_answer.sh
+++ run_answer.sh
@@ -17,7 +17,7 @@
 # python answer_qa.py "tedlium" "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "gpt" "gpt-4o"
 # python answer_qa.py "voxpopuli" "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "gpt" "gpt-4o"
 
-python answer_qa.py "voxpopuli" "whisper-large-v2" "whisper_v2_1best" "vicuna" "lmsys/vicuna-13b-v1.1"
-#python answer_qa.py "earning22" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
-#python answer_qa.py "medasr" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
-#python answer_qa.py "tedlium" "whisper-large-v2" "whisper_v2_1best" "qwen-7b" "Qwen/Qwen-7B-Chat"
+python answer_qa.py "voxpopuli" "whisper-large-v3" "whisper_v3_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
+python answer_qa.py "earning22" "whisper-large-v3" "whisper_v3_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
+python answer_qa.py "medasr" "whisper-large-v3" "whisper_v3_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
+python answer_qa.py "tedlium" "whisper-large-v3" "whisper_v3_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
