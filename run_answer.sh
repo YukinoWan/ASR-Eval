a18c7dc6a1265a39333a96bd4de3a247c528f8c1
zhenwan.nlp <zhenwan.nlp@cnode7-013.cm.cluster>
2025-02-07 00:40:42 +0800

    espnet llm

--- run_answer.sh
+++ run_answer.sh
@@ -12,10 +12,10 @@
 # python answer_qa.py "medasr" "canary" "canary_1best" "gpt" "gpt-4o"
 
 
- python answer_qa.py "medasr" "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
- python answer_qa.py "earning22" "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
- python answer_qa.py "tedlium" "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
- python answer_qa.py "voxpopuli" "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
+ python answer_qa.py "medasr" "espnet" "espnet_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
+ python answer_qa.py "earning22" "espnet" "espnet_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
+ python answer_qa.py "tedlium" "espnet" "espnet_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
+ python answer_qa.py "voxpopuli" "espnet" "espnet_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
 
 #python answer_qa.py "voxpopuli" "canary" "canary_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
 #python answer_qa.py "tedlium" "canary" "canary_1best" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
--- run_llmeval.sh
+++ run_llmeval.sh
@@ -1,4 +1,4 @@
-# python llm_respond.py "earning22" "qwen2-audio" "qwen2-audio"
-python llm_respond.py "medasr" "qwen2-audio" "qwen2-audio"
-python llm_respond.py "tedlium" "qwen2-audio" "qwen2-audio"
-python llm_respond.py "voxpopuli" "qwen2-audio" "qwen2-audio"
+python llm_respond.py "earning22" "espnet" "espnet_1best"
+python llm_respond.py "medasr" "espnet" "espnet_1best"
+python llm_respond.py "tedlium" "espnet" "espnet_1best"
+python llm_respond.py "voxpopuli" "espnet" "espnet_1best"
