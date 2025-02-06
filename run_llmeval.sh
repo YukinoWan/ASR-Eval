8eb69e9bf7e787ad91b61e723208247ed6f4549d
zhenwan.nlp <zhenwan.nlp@cnode7-013.cm.cluster>
2025-02-07 01:32:22 +0800

    gemini-flash-llm

--- run_llmeval.sh
+++ run_llmeval.sh
@@ -1,4 +1,4 @@
-python llm_respond.py "earning22" "espnet" "espnet_1best"
-python llm_respond.py "medasr" "espnet" "espnet_1best"
-python llm_respond.py "tedlium" "espnet" "espnet_1best"
-python llm_respond.py "voxpopuli" "espnet" "espnet_1best"
+python llm_respond.py "earning22" "gemini-1.5-flash" "gemini-1.5-flash"
+python llm_respond.py "medasr" "gemini-1.5-flash" "gemini-1.5-flash"
+python llm_respond.py "tedlium" "gemini-1.5-flash" "gemini-1.5-flash"
+python llm_respond.py "voxpopuli" "gemini-1.5-flash" "gemini-1.5-flash"
