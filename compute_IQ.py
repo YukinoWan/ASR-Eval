db8892d2d17c991a9ea7da5769306e842dd1d59f
YukinoWan <Zhen_Wan@outlook.com>
2025-02-14 21:29:35 +0900

    baichuan anygpt

--- compute_IQ.py
+++ compute_IQ.py
@@ -131,16 +131,18 @@ def select_higher(a, b):
 def select_avg(a, b, c):
     return (a + b + c) / 3
 if __name__ == "__main__":
-    models = ["canary_gpt-4o", "canary_qwen2-7b", "desta2_desta2", "qwen2-audio", "whisper_v2_nbest_gpt4o_gpt-4o", "whisper_v2_nbest_gpt4o_qwen2-7b", "whisper-large-v2_gpt-4o", "whisper-large-v2_qwen2-7b", "whisper-large-v3_gpt-4o","whisper-large-v3_qwen2-7b", "gemini-1.5-flash_gemini-1.5-flash", "gemini-1.5-pro_gemini-1.5-pro", "espnet_qwen2-7b"]
-    # datasets = ["earning22", "tedlium", "voxpopuli", "medasr"]
-    datasets = ["tedlium"]
+    # models = ["canary_gpt-4o", "canary_qwen2-7b", "desta2_desta2", "qwen2-audio", "whisper_v2_nbest_gpt4o_gpt-4o", "whisper_v2_nbest_gpt4o_qwen2-7b", "whisper-large-v2_gpt-4o", "whisper-large-v2_qwen2-7b", "whisper-large-v3_gpt-4o","whisper-large-v3_qwen2-7b", "gemini-1.5-flash_gemini-1.5-flash", "gemini-1.5-pro_gemini-1.5-pro", "espnet_qwen2-7b", "salmonn_salmonn"]
+    models = ["canary_qwen2-7b", "desta2_desta2", "qwen2-audio", "whisper_v2_nbest_gpt4o_qwen2-7b", "whisper-large-v2_qwen2-7b","whisper-large-v3_qwen2-7b", "gemini-1.5-flash_gemini-1.5-flash", "gemini-1.5-pro_gemini-1.5-pro", "espnet_qwen2-7b", "salmonn_salmonn"]
+
+    datasets = ["earning22", "voxpopuli", "medasr"]
+    # datasets = ["medasr"]
     # 假设我们有 3 组模型 Size（小、中、大），以及 3 个维度（WER, Similarity, QA Accuracy）
     WER = []
     SIM = []
     QA = []
-    compute_variance_weights_dimensions = True
+    compute_variance_weights_dimensions = False
     for model in models:
-        data = json.load(open(f"/home/zhen/ASR-Eval/analysis/subset/{model}.json", "r", encoding="utf-8"))
+        data = json.load(open(f"./analysis/subset/{model}.json", "r", encoding="utf-8"))
         WER.append([-d["WER"] for d in data if d["dataset"] in datasets])
         SIM.append([select_lower(d["LLM_BACK"], d["LLM_SUMMARIZE"])for d in data if d["dataset"] in datasets])
         # SIM.append([d["LLM_AVG"] for d in data if d["dataset"] in datasets])
@@ -162,9 +164,9 @@ if __name__ == "__main__":
     # 假设模型 Size（0: 小, 1: 中, 2: 大）
     # model_sizes = np.array([1, 0, 0, 1, 1, 1])  # 6 个模型分别属于 3 组 Size
 
-    model_sizes_WER = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # WER 维度
-    model_sizes_SIM = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # SIM 维度
-    model_sizes_QA  = np.array([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])  # QA 维度
+    model_sizes_WER = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # WER 维度
+    model_sizes_SIM = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # SIM 维度
+    model_sizes_QA  = np.array([2, 2, 2, 2, 2, 2, 2, 2, 2, 2])  # QA 维度
 
 
     # 计算各维度的区分度（方差权重）
@@ -198,7 +200,7 @@ if __name__ == "__main__":
         final_scores_var = weights_var[0] * WER_size_adjusted + weights_var[1] * SIM_size_adjusted + weights_var[2] * QA_size_adjusted
     else:
         # w1, w2, w3 = 0.03, 0.72, 0.25  # 维度权重
-        w1, w2, w3 = 0.02, 0.9, 0.08 # 维度权重
+        w1, w2, w3 = 0.04, 0.7, 0.26 # 维度权重
         final_scores_var = w1 * WER_size_adjusted + w2 * SIM_size_adjusted + w3 * QA_size_adjusted
     IQ_scores_var = convert_to_iq(final_scores_var)
 
--- run_analysis.sh
+++ run_analysis.sh
@@ -12,7 +12,7 @@
 # python score_stat.py "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "qwen2-7b" "Qwen/Qwen2-7B-Instruct"
 # python score_stat.py "whisper_v2_nbest_gpt4o" "whisper_v2_nbest_gpt4o" "gpt-4o" "gpt-4o"
 
-# python score_stat.py "desta2" "desta2" "desta2" "desta2"
-python score_stat.py "gemini-1.5-flash" "gemini-1.5-flash" "gemini-1.5-flash" "gemini-1.5-flash"
+python score_stat.py "salmonn" "salmonn" "salmonn" "salmonn"
+# python score_stat.py "gemini-1.5-flash" "gemini-1.5-flash" "gemini-1.5-flash" "gemini-1.5-flash"
 # python score_stat.py "qwen2-7b" "qwen2-7b" "qwen2-7b" "qwen2-7b"
 
